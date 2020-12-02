afd7ab574017c325f31df274764ccdea1c9358f8
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-02 17:46:32 +0900

    install ui bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/tests/test', 'TestController@index');
 
 Route::get('/tests/test2', 'Test2Controller@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
