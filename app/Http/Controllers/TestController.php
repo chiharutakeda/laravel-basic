be6c1982e57fae8de40dbf3a5bced7387f0987a7
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-02 16:36:14 +0900

    query-builder

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,12 +6,19 @@ use Illuminate\Http\Request;
 
 use App\Models\Test;
 
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
     public function index(){
 
+        //条件が荒い時
         $values = Test::all();
 
+        //ちょっと条件が複雑な時クエリビルダを使う（さらに複雑な場合は生sqlを書くことになる）
+        $tests = DB::table('tests')
+        ->select('id')->get();
+        dd($tests);
         // dd($values);
 
         return view('tests.test',compact('values'));
